a6fe9a50f2df8ff0a3cfd538c6354b5d5cfd8012
fable-dev <lucienfable999@gmail.com>
2025-12-13 12:23:25 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,12 @@
+// data.js
+const POKEMON_MOVESETS = {
+  rayquaza: {
+    fast: ["dragon_tail"],
+    charged: ["outrage", "breaking_swipe"]
+  },
+  mega_rayquaza: {
+    fast: ["dragon_tail"],
+    charged: ["outrage"]
+  }
+};
 
