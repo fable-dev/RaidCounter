cdc906d44c4c37cb98512a19e2051a2f1118f8bb
fable-dev <lucienfable999@gmail.com>
2025-12-13 12:35:31 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,37 @@
-// data.js
+// Move sets for a small pool of meta-ish attackers.
+// The keys must match pokemon_stats.json "id" fields.
+
 const POKEMON_MOVESETS = {
-  rayquaza: {
-    fast: ["dragon_tail"],
-    charged: ["outrage", "breaking_swipe"]
+  mamoswine: {
+    fast: ["powder_snow"],
+    charged: ["avalanche"]
   },
-  mega_rayquaza: {
+  weavile: {
+    fast: ["snarl"],
+    charged: ["avalanche"]
+  },
+  galarian_darmanitan: {
+    fast: ["ice_fang"],
+    charged: ["avalanche"]
+  },
+  rampardos: {
+    fast: ["smack_down"],
+    charged: ["rock_slide"]
+  },
+  raikou: {
+    fast: ["thunder_shock"],
+    charged: ["wild_charge"]
+  },
+  roserade: {
+    fast: ["razor_leaf"],
+    charged: ["grass_knot"]
+  },
+  kyogre_attacker: {
+    fast: ["waterfall"],
+    charged: ["hydro_pump"]
+  },
+  rayquaza_attacker: {
     fast: ["dragon_tail"],
     charged: ["outrage"]
   }
 };
-
